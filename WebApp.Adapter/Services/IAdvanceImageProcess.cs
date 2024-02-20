191ed716deff4582429042c53d1ceef29e943060
ebsagdic42 <84442369+ebsagdic42@users.noreply.github.com>
2024-02-20 13:28:18 +0300

    design patterns

---
+++ WebApp.Adapter/Services/IAdvanceImageProcess.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace WebAppAdapter.Services
+{
+    public interface IAdvanceImageProcess
+    {
+        void AddWaterMarkImage(Stream stream, string text, string filePath, Color color, Color outlineColor);
+        
+    }
+}
---
+++ WebApp.ChainOfResponsibility/ChainOfResponsibility/ExcelProcessHandler.cs
@@ -0,0 +1,44 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+
+namespace WebApp.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ExcelProcessHandler<T>:ProcessHandler
+    {
+        private DataTable GetTable(Object o)
+        {
+            DataTable table = new DataTable();
+
+            var type = typeof(T);
+
+            type.GetProperties().ToList().ForEach(x => table.Columns.Add(x.Name, x.PropertyType));
+
+            var list = o as List<T>;
+
+            list.ForEach(x =>
+            {
+                var values = type.GetProperties().Select(propertyInfo => propertyInfo.GetValue(x, null)).ToArray();
+
+                table.Rows.Add(values);
+            });
+
+            return table;
+        }
+
+        public override object Handle(object o)
+        {
+            var wb = new XLWorkbook();
+            var ds = new DataSet();
+
+            ds.Tables.Add(GetTable(o));
+
+            wb.Worksheets.Add(ds);
+
+            var excelMemoryStream = new MemoryStream();
+
+            wb.SaveAs(excelMemoryStream);
+
+            return base.Handle(excelMemoryStream);
+        }
+    }
+}
